3d1ea0d1360dea3b000dde894346f8c4d1b45fec
Sophia Joy Wang <wangsj@fb.com>
2021-06-23 08:49:17 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by Sophia Joy Wang on 6/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Flix/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Flix
+//
+//  Created by Sophia Joy Wang on 6/23/21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
